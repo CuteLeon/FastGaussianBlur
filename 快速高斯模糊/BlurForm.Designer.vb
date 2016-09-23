68ef93d37879c9046e1d74040f75bb20c95f4e3e
Leon <xiaoyankeji@hotmail.com>
2016-09-23 16:10:31 +0800

    第一次提交

---
+++ 快速高斯模糊/BlurForm.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BlurForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(BlurForm))
+        Me.RadiusBar = New System.Windows.Forms.TrackBar()
+        CType(Me.RadiusBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadiusBar
+        '
+        Me.RadiusBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.RadiusBar.LargeChange = 15
+        Me.RadiusBar.Location = New System.Drawing.Point(0, 0)
+        Me.RadiusBar.Maximum = 272
+        Me.RadiusBar.Name = "RadiusBar"
+        Me.RadiusBar.Size = New System.Drawing.Size(553, 45)
+        Me.RadiusBar.SmallChange = 5
+        Me.RadiusBar.TabIndex = 0
+        Me.RadiusBar.TickFrequency = 5
+        Me.RadiusBar.Value = 20
+        '
+        'BlurForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(553, 554)
+        Me.Controls.Add(Me.RadiusBar)
+        Me.DoubleBuffered = True
+        Me.Name = "BlurForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "快速高斯模糊"
+        CType(Me.RadiusBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadiusBar As TrackBar
+End Class
---
+++ 快速高斯模糊/BlurForm.vb
@@ -0,0 +1,24 @@
+﻿Public Class BlurForm
+    Dim OriginalBitmap As Bitmap
+
+    Private Sub RadiusBar_Scroll(sender As Object, e As EventArgs) Handles RadiusBar.Scroll
+        Dim FirstTime As Integer = My.Computer.Clock.TickCount
+        Dim MyGaussianBlur As FastGaussianBlur = New FastGaussianBlur(OriginalBitmap)
+        BackgroundImage = MyGaussianBlur.GaussianBlur(RadiusBar.Value)
+        Dim SecondTime As Integer = My.Computer.Clock.TickCount
+        Me.Text = "快速高斯模糊 - 模糊半径：" & RadiusBar.Value & "，用时：" & (SecondTime - FirstTime) & " 毫秒"
+        MyGaussianBlur = Nothing
+        GC.Collect()
+    End Sub
+
+    Private Sub BlurForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        OriginalBitmap = Me.BackgroundImage
+        RadiusBar_Scroll(sender, e)
+    End Sub
+
+    Private Sub BlurForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Dim FileName As String = "D:\GaussianBlur-" & RadiusBar.Value & "-" & My.Computer.Clock.TickCount & ".png"
+        Me.BackgroundImage.Save(FileName)
+        Me.Text = "文件已经储存在 " & FileName
+    End Sub
+End Class
